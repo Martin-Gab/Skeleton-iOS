6e877ff3833de9b60286f5929e5ecc617206a02f
Martin-Gab <mgabillet@dev-id.fr>
2025-01-18 13:43:44 +0100

    initial project

---
+++ Skeleton-iOS/Skeleton_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Skeleton_iOSApp.swift
+//  Skeleton-iOS
+//
+//  Created by Martin Gabillet on 18/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct Skeleton_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
